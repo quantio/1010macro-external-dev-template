5ab7fdc2bb4e5845db0cffff16268894ae48be1e
jcristini <jamie.cristini@tech-antics.com>
2017-04-17 18:33:04 -0700

    fixed teamcity reporter config

--- tasks/configure.jasmine_nodejs.js
+++ tasks/configure.jasmine_nodejs.js
@@ -2,10 +2,14 @@ module.exports = function (grunt) {
   var teamcity = grunt.option("teamcity") === true;
 
   return {
+    options: {
+      reporters: {
+        teamcity: teamcity
+      }
+    },
     test_queries: {
       options: {
-        useHelpers: true,
-        teamcity: teamcity
+        useHelpers: true
       },
       specs: [
         "build/**/*.spec.js"
